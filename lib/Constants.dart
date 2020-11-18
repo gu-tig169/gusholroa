8e3bccf1d98829e6b78b293c610ecc00c30fb3f4
renaissanceMan92 <timholm.contact@gmail.com>
2020-11-18 17:55:08 +0100

    Steg 1.

---
+++ lib/Constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+
+  static const All = 'all';
+  static const Done = 'done';
+  static const Undone = 'undone';
+  static const ChoiceItems = <String>[All, Done, Undone];
+
+}
---
+++ lib/Item.dart
@@ -0,0 +1,8 @@
+class Item {
+
+  int id;
+  String name;
+  bool isChecked;
+  Item(this.id, this.name, this.isChecked);
+
+}
